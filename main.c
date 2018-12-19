95e2a9c36871610dfaddbd9884c1231a3843e4c8
Moein Ghandehari <moeinghandehari@gmail.com>
2018-12-19 22:45:28 +0100

    Completed

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int menu();
 
 void bubbleSort(int *, int);
 
@@ -6,13 +9,70 @@ void merge(int arr[], int, int, int);
 
 void mergeSort(int *, int, int);
 
+int wahl;
+int array_groesse;
+int *array;
+
 int main() {
+    while (1) {
+        menu();
+    }
     return 0;
+}
+
+int menu() {
+    //Menu zeigen
+    //
+    printf("\n1) Array-Groesse festlegen");
+    printf("\n2) Zahlen von Tastatur einlesen");
+    printf("\n3) Zufallszahlen erzeugen");
+    printf("\n4) Bubblesort (downwards)");
+    printf("\n5) Mergesort");
+    printf("\n6) Print Array");
+    printf("\n7) Programm beenden");
+    printf("\n");
+    scanf("%d", &wahl);
+    switch (wahl) {
+        case 1:
+            printf("\nGeben Sie die Groesse des Arrays ein: ");
+            scanf("%d", &array_groesse);
+            array = malloc(array_groesse * sizeof(int));
+            break;
+        case 2:
+            printf("Geben Sie die Zahlen ein");
+            for (int i = 0; i < array_groesse; ++i) {
+                printf("\nGeben Sie die %d. Zahl ein: ", (i + 1));
+                int temp;
+                scanf("%d", &temp);
+                array[i] = temp;
+            }
+            break;
+        case 3:
+            for (int j = 0; j < array_groesse; ++j)
+                array[j] = rand() % 1000;
+            break;
+        case 4:
+            bubbleSort(array, array_groesse);
+            break;
+        case 5:
+            mergeSort(array, 0, array_groesse - 1);
+            break;
+        case 6:
+            printf("\n");
+            for (int j = 0; j < array_groesse; ++j) {
+                printf("%d - ", array[j]);
+            }
+            break;
+        case 7:
+            return 0;
+        default:
+            break;
+    }
 }
 
 void bubbleSort(int *array, int length) {
     for (int i = 0; i < length - 1; ++i) {
-        for (int j = 0; j < length - 2; ++j) {
+        for (int j = 0; j < length - i - 1; ++j) {
             if (array[j] > array[j + 1]) {
                 int temp = array[j];
                 array[j] = array[j + 1];
@@ -22,7 +82,7 @@ void bubbleSort(int *array, int length) {
     }
 }
 
-void merge(int arr[], int l, int m, int r) {
+void merge(int *arr, int l, int m, int r) {
     int i, j, k;
     int n1 = m - l + 1;
     int n2 = r - m;
